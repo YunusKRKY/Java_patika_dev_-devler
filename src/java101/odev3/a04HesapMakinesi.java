1d25e49ef6828c8da8da14a403ef82e339f6577a
kullanıcı adı / rumuz <E-posta>
2022-12-08 23:31:09 +0100

    commit

---
+++ src/java101/odev3/a04HesapMakinesi.java
@@ -0,0 +1,30 @@
+package java101.odev3;
+
+import java.util.Scanner;
+
+public class a04HesapMakinesi {
+
+
+    /*
+    #Java ile girilen 2 sayı ile kullanıcının seçtiği 4 işlemleri yapabilen bir hesap makinesi yapınız.
+     */
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("lütfen işlem yapmak istediğiniz ilk sayıyı giriniz : ");
+        double ilkSayi= scanner.nextInt();
+        System.out.print("lütfen işlem yapmak istediğiniz ikinci sayıyı giriniz : ");
+        double ikinciSayi= scanner.nextInt();
+        System.out.print("hangi işlemi yapacağınızı seçiniz: ");
+        String islem=scanner.next();
+
+        switch (islem) {
+            case "+" -> System.out.println("işlem sonucu: " + (ilkSayi + ikinciSayi));
+            case "-" -> System.out.println("işlem sonucu: " + (ilkSayi - ikinciSayi));
+            case "*" -> System.out.println("işlem sonucu: " + (ilkSayi * ikinciSayi));
+            case "/" -> System.out.println("işlem sonucu: " + (ilkSayi / ikinciSayi));
+        }
+
+
+    }
+}
